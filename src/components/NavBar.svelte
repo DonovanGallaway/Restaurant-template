fbeefc9fe87c4db5a7b67de18e1034e539fac6a4
David <Davidfriedchicken@gmail.com>
2022-07-28 20:01:31 -0700

    Nav bar almost finished

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -6,76 +6,111 @@
 
     let burgerTruthy = false
     let rotation = 0
+    let vh = 0
+    let display
+    let position
+    let height
 
     let screenWidth = document.documentElement.clientWidth;
+
+    if(screenWidth <= 600) {
+        display = "none"
+    }  else {
+        display = "block"
+    }
 
     const burgerFunc = () => {
         if(burgerTruthy === false) {
             burgerTruthy = true
             rotation = 90
+            display = "block"
+            height = window.innerHeight - 304
+            console.log(height)
+            position = "relative"
         } else {
             burgerTruthy = false
             rotation = 0
+            display = "none"
+            height = 0
+            position = "absolute"
         }
     }
 
-    window.addEventListener("resize", () => {screenWidth = document.documentElement.clientWidth})
+    console.log(window.innerHeight)
 
-    console.log(screenWidth)
+    
+
+    const resizeFunc = () => {
+        screenWidth = document.documentElement.clientWidth
+        if(screenWidth <= 600 && burgerTruthy === false) {
+            display = "none"
+            position = "absolute"
+        }  else {
+            display = "block"
+            burgerTruthy = false
+            position = "relative"
+        }
+    }
+
+    window.addEventListener("resize", resizeFunc)
 
 </script>
 
+<div id="Nav">
 <nav>
-
-    <div id="nav-items"
-     class:burger={screenWidth <= 600}
-     class:noBurger={screenWidth > 600}
-     >
-
-
-    <h1 
-        class="nav-item" 
-        class:hidden={screenWidth <= 600 && burgerTruthy === false}
-        class:visible={screenWidth <= 600 && burgerTruthy}
-        >About us
-    </h1>
-
-
-
-
-    <h1 
-        class="nav-item" 
-        class:hidden={screenWidth <= 600 && burgerTruthy === false}
-        class:visible={screenWidth <= 600 && burgerTruthy}
-        >Menu
-    </h1>
-
-
-
-
-    <h1 
-        class="nav-item" 
-        class:hidden={screenWidth <= 600 && burgerTruthy === false}
-        class:visible={screenWidth <= 600 && burgerTruthy}
-        >Third page
-    </h1>
 
 
 
     <div id="hamburger" class:burgerInvisible={screenWidth > 600} class:burgerVisible={screenWidth <= 600} on:click={burgerFunc}>
         <Fa icon={faBars} scale={3} style="
-        margin-top: 25px;
+        margin-top: 30px;
         transform: rotate({rotation}deg);
         color: white;
         transition: 0.5s;
         " />
     </div>
-    </div>
+    
 
     
 </nav>
 
+
+<div id="nav-items"
+    style="padding-bottom: {height}px;"
+    class:extend={burgerTruthy === true}
+    class:retract={burgerTruthy === false}
+>
+
+
+<h1 
+   class="nav-item" 
+   style="display: {display}; position: {position}">
+   About us
+</h1>
+
+
+
+
+<h1 
+   class="nav-item" 
+   style="display: {display}; position: {position}">
+   Menu
+</h1>
+
+
+
+
+<h1 
+   class="nav-item" 
+   style="display: {display}; position: {position}">
+   Third page
+</h1>
+</div>
+
+</div>
+
 <style>
+
     nav {
         position: relative;
         top: 0;
@@ -83,68 +118,76 @@
         background: black;
         padding-bottom: 100px
     }
+    
+    .extend {
+        transition: 0.5s;
 
-    .noItems {
-        padding-bottom: 70px;
     }
 
-    #nav-items {
-        position: absolute;
-    }
-
-    .nav-item {
-        
-    }
 
     .burger {
 
     }
 
     .noBurger {
+
+    }
+
+
+    /*Character stuff*/
+
+    #nav-items {
+        position: absolute;
         display: flex;
         justify-content: center;
+        top: 0;
+        background: black;
         width: 100%;
     }
 
-    .item-container {
-        display: block;
-    }
+
+
 
     h1 {
-        margin: 20px;
-        margin-left: 5%;
-        margin-right: 5%;
+        margin: 30px 5%;
         color: white
     }
 
-    .burgerInvisible {
+
+
+    /*Burger stuff*/
+    #hamburger {
         position: absolute;
-        right: 10%;
-        opacity: 0
+        display: none
     }
 
     .burgerVisible {
-        display: block
+        display: block;
+        right: 35px
     }
 
-    .hidden {
-        opacity: 0
-    }
+
 
 
     @media only screen and (max-width: 600px) {
-        /*#hamburger {
+        #hamburger {
             display: block;
-            position: absolute;
             right: 10%;
         }
 
-        .nav-item {
-            display: none
+        #nav-items {
+            display: block;
+            top: 100px;
+            right: 0;
+   
+            /* height: 87vh; */
+            width:20%;
+            background: black;
+            transition: 0.5s;
         }
 
-        nav {
-            padding-bottom: 70px
-        }*/
+        h1 {
+            font-size: medium;
+        }
     }
 </style>
