19429e14c259d76666d87c89941ccb842d526019
David <Davidfriedchicken@gmail.com>
2022-08-07 21:28:16 -0700

    Anewed navBar code

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -1,6 +1,7 @@
 <script>
     import Fa from 'svelte-fa'
     import { faBars } from '@fortawesome/free-solid-svg-icons'
+    import { fade, blur, fly, slide, scale, crossfade } from 'svelte/transition';
     import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 
@@ -8,56 +9,50 @@
     let rotation = 0
     let opacity
     //let position
-    let height
+    let height = document.documentElement.clientHeight * 0.90
     let marginTop
     let marginHelper
+    let position
+    let burgerSize = 3
+
+    let navBar = false
 
     let screenWidth = document.documentElement.clientWidth;
 
     if(screenWidth <= 600) {
-        opacity = 0
+        navBar = true
     }  else {
-        opacity = 1
+        navBar = false
 
     }
+
+    if(document.documentElement.clientHeight <= 500) {
+        burgerSize = 2
+    }  
 
     const burgerFunc = () => {
         if(burgerTruthy === false) {
             burgerTruthy = true
             rotation = 90
-            opacity = 1
-            height = window.innerHeight - 100
+            //height = window.innerHeight
             console.log(height)
-            //position = "relative"
-            marginTop = 50
-            marginHelper = 50
         } else {
             burgerTruthy = false
             rotation = 0
-            opacity = 0
-            height = 0
-            marginTop = 0
-            marginHelper = 0
-            //position = "absolute"
-            
         }
     }
 
-    console.log(window.innerHeight)
+    console.log(document.documentElement.clientHeight * 0.65)
 
     
 
     const resizeFunc = () => {
         screenWidth = document.documentElement.clientWidth
         if(screenWidth <= 600 && burgerTruthy === false) {
-            opacity = 0
-            position = "absolute"
+            navBar = true
         }  else {
-            opacity = 1
-            marginHelper = 0
-            marginTop = 30
+            navBar = false
             burgerTruthy === true ? burgerFunc() : burgerTruthy = false
-            position = "relative"
         }
     }
 
@@ -65,26 +60,145 @@
 
 </script>
 
-<div id="Nav">
+<div id="stickyNav">
 <nav>
 
 
-
+    {#if navBar}
     <div id="hamburger" style="padding: 10px" on:click={burgerFunc}>
-        <Fa icon={faBars} scale={3} style="
-            margin-top: 30px;
+        <Fa icon={faBars} scale={burgerSize} style="
             transform: rotate({rotation}deg);
             color: white;
             transition: 0.5s;
         " />
     </div>
-    
 
-    
+    {:else}
+    <div class="nav-items">
+        <h1 class="nav-item">About us</h1>
+        <h1 class="nav-item">Menu</h1>
+        <h1 class="nav-item">another page</h1>
+    </div>
+
+    {/if}
+
 </nav>
 
 
-<div id="nav-items"
+</div>
+
+{#if burgerTruthy}
+    <div class="burgerContent" transition:fly={{y: -height, duration: 500, opacity: 1}}>
+        <h3 on:click={() => console.log('about me was clicked')} class="hambuger-item">About me</h3>
+        <h3 class="hambuger-item">menu</h3>
+        <h3 class="hambuger-item">Another page</h3>
+    </div>
+{/if}
+
+
+
+<style>
+
+    #stickyNav {
+        position: fixed;
+        width: 100%;
+        z-index: 5;
+    }
+
+    nav {
+        position: relative;
+        top: 0;
+        left: 0;
+        background: black;
+        padding-bottom: 15vh;
+        z-index: 2;
+    }
+
+    .nav-items {
+        position: absolute;
+        display: flex;
+        top: 50%;
+        transform: translateY(-50%);
+        width: 100%;
+    }
+
+    .nav-item {
+        color: white;
+        display: inline;
+        margin: auto;
+    }
+
+    .burgerContent {
+        position: fixed;
+        margin-top: 20%;
+        width: 25%;
+        height: 85vh;
+        bottom: 0;
+        background: black;
+        right: 0;
+        z-index: 1;
+    }
+
+    .hambuger-item {
+        color: white;
+        margin: 5px;
+        margin-top: 20px;
+    }
+
+    /*Burger stuff*/
+    #hamburger {
+        position: absolute;
+        display: none;
+        top: 50%;
+        transform: translateY(-50%);
+        margin: 0;
+        z-index: 2;
+    }
+
+
+    #test {
+        position: relative;
+        top: 100px;
+        padding-bottom: 200vh;
+        width: 100%;
+        background: pink;
+
+    }
+
+    #test2 {
+        width: 100%;
+        height: 20vh;
+        background-color: blue;
+    }
+
+
+    @media only screen and (max-width: 600px) {
+        #hamburger {
+            display: block;
+            right: 7%;
+        }
+    }
+
+    
+    @media only screen and (max-width: 400px) {
+        .burgerContent {
+            width: 40%;
+        }
+
+        #hamburger {
+            right: 14%
+        }
+    }
+
+    @media only screen and (max-height: 500px) { 
+        nav {
+            padding-bottom: 70px
+        }
+    }
+</style>
+
+
+<!--<div id="nav-items"
     
     style="padding-bottom: {height}px;"
 >
@@ -123,96 +237,4 @@
 </h1>
 </div>
 
-</div>
-
-<div id="test"></div>
-<div id="test2"></div>
-
-<style>
-
-    nav {
-        position: relative;
-        top: 0;
-        left: 0;
-        background: black;
-        padding-bottom: 100px
-    }
-
-    #Nav {
-        position: fixed;
-        width: 100%;
-        z-index: 10;
-    }
-
-    #test {
-        position: relative;
-        top: 100px;
-        padding-bottom: 200vh;
-        width: 100%;
-        background: pink;
-
-    }
-
-    #test2 {
-        width: 100%;
-        height: 20vh;
-        background-color: blue;
-    }
-
-
-
-    /*Character stuff*/
-
-    #nav-items {
-        position: absolute;
-        display: flex;
-        justify-content: center;
-        top: 0;
-        background: black;
-        width: 100%;
-    }
-
-
-
-
-
-
-    h1 {
-        margin: 30px 5%;
-        color: white;
-    }
-
-
-
-    /*Burger stuff*/
-    #hamburger {
-        position: absolute;
-        display: none
-    }
-
-
-
-
-    @media only screen and (max-width: 600px) {
-        #hamburger {
-            display: block;
-            right: 10%;
-        }
-
-        #nav-items {
-            display: block;
-            top: 100px;
-            right: 0;
-   
-            width:25%;
-            background: black;
-            transition: 0.5s;
-        }
-
-        h1 {
-            font-size: medium;
-            position: absolute;
-            transition: 0.3s;
-        }
-    }
-</style>
+</div>-->
